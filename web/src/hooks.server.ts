c6497582fba2a4d20634b19b67dde457592c105f
Leon Scherer <info@leonscherer.com>
2024-11-06 18:33:21 +0100

    make auth check optional

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -4,12 +4,12 @@ import { env } from '$env/dynamic/private'
 
 await api.init()
 
-if (!env.ADMIN_AUTH) throw new Error('ADMIN_AUTH is not set')
-
-const ADMIN_AUTH = env.ADMIN_AUTH.split(',').map((auth) => Buffer.from(auth).toString('base64'))
+const ADMIN_AUTH = env.ADMIN_AUTH
+	? env.ADMIN_AUTH.split(',').map((auth) => Buffer.from(auth).toString('base64'))
+	: null
 
 export const handle: Handle = async ({ event, resolve }) => {
-	if (event.url.pathname.startsWith('/admin')) {
+	if (ADMIN_AUTH && event.url.pathname.startsWith('/admin')) {
 		const auth = event.request.headers.get('authorization')
 
 		if (!auth || !ADMIN_AUTH.some((adminAuth) => auth === `Basic ${adminAuth}`)) {
