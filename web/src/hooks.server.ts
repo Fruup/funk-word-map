4a231f53b9edc93dc0d6f7181d074838759916f0
Leon Scherer <info@leonscherer.com>
2024-11-07 23:54:17 +0100

    chore: proxy sentry endpoints

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -2,9 +2,10 @@ import { initializeSentry } from '$lib/errors'
 import { Sentry } from '$lib/errors/server'
 initializeSentry(Sentry)
 
-import { type Handle, type HandleServerError } from '@sveltejs/kit'
+import { type Handle, type HandleServerError, type RequestEvent } from '@sveltejs/kit'
 import { api } from './lib/server/api'
 import { env } from '$env/dynamic/private'
+import { env as publicEnv } from '$env/dynamic/public'
 
 await api.init()
 
@@ -12,7 +13,27 @@ const ADMIN_AUTH = env.ADMIN_AUTH
 	? env.ADMIN_AUTH.split(',').map((auth) => Buffer.from(auth).toString('base64'))
 	: null
 
+const proxy = proxyHandle(
+	{
+		/**
+		 * Proxy sentry endpoints to monitoring service.
+		 * Prevents the requests from being blocked.
+		 */
+		[`/api/${publicEnv.PUBLIC_SENTRY_DSN_PROJECT_ID}/envelope`]:
+			'https://monitoring.leonscherer.com',
+	},
+	{
+		changeOrigin: true,
+		credentials: 'include',
+	},
+)
+
 export const handle: Handle = async ({ event, resolve }) => {
+	// Catch sentry requests.
+	const proxiedResponse = await proxy(event)
+	if (proxiedResponse) return proxiedResponse
+
+	// Secure routes.
 	if (ADMIN_AUTH && event.url.pathname.startsWith('/admin')) {
 		const auth = event.request.headers.get('authorization')
 
@@ -38,5 +59,74 @@ export const handleError: HandleServerError = ({ error, event, message, status }
 		status,
 		message,
 		errorId,
+	}
+}
+
+/**
+ * Extension of https://github.com/born05/sveltekit-proxy.
+ */
+export function proxyHandle(
+	proxy: { [key: string]: string },
+	options: { debug?: boolean; changeOrigin?: boolean; credentials?: RequestInit['credentials'] } = {
+		changeOrigin: true,
+	},
+): (event: RequestEvent) => Promise<Response | undefined> {
+	return async function (event) {
+		const { url, request } = event
+		const { pathname, search } = url
+
+		/**
+		 * Find first matching path
+		 */
+		const matchingProxy = Object.keys(proxy).find((proxyPath) => pathname.match(proxyPath))
+		if (!matchingProxy) return
+
+		const proxyTarget = proxy[matchingProxy]
+
+		/**
+		 * Collect request headers
+		 */
+		const requestHeaders = new Headers(request.headers)
+		if (options.changeOrigin) {
+			requestHeaders.delete('host')
+		}
+
+		if (options.debug) {
+			console.debug(`Proxy: ${proxyTarget}${pathname}`, requestHeaders)
+		}
+
+		/**
+		 * Fetch data from remote server
+		 */
+		try {
+			const response = await fetch(`${proxyTarget}${pathname}${search}`, {
+				redirect: 'manual',
+				method: request.method,
+				headers: requestHeaders,
+				body: await request.arrayBuffer(),
+				credentials: options.credentials,
+			})
+
+			/**
+			 * Clean up response headers
+			 */
+			const responseHeaders = new Headers(response.headers)
+			responseHeaders.delete('content-encoding')
+
+			if (options.debug) {
+				console.debug(`Proxy response (${response.status}) headers:`, responseHeaders)
+			}
+
+			/**
+			 * Return response from remote server
+			 */
+			return new Response(response.body, {
+				status: response.status,
+				statusText: response.statusText,
+				headers: responseHeaders,
+			})
+		} catch (error) {
+			console.error(error)
+		}
 	}
 }
--- web/src/lib/errors/index.ts
+++ web/src/lib/errors/index.ts
@@ -1,6 +1,7 @@
 import pkg from '../../../package.json'
 import { env } from '$env/dynamic/public'
 import type { init, isInitialized } from '@sentry/browser'
+import { browser } from '$app/environment'
 
 export const initializeSentry = (instance: {
 	isInitialized: typeof isInitialized
@@ -10,8 +11,16 @@ export const initializeSentry = (instance: {
 
 	console.log('Initializing Sentry...')
 
+	const { protocol, host } = new URL(env.PUBLIC_ORIGIN)
+
+	const dsn = browser
+		? `${protocol}//${env.PUBLIC_SENTRY_DSN_KEY}@${host}/${env.PUBLIC_SENTRY_DSN_PROJECT_ID}` // Proxy
+		: `https://${env.PUBLIC_SENTRY_DSN_KEY}@${env.PUBLIC_SENTRY_DSN_DOMAIN}/${env.PUBLIC_SENTRY_DSN_PROJECT_ID}`
+
+	console.log({ dsn })
+
 	instance.init({
-		dsn: env.PUBLIC_SENTRY_DSN,
+		dsn,
 		environment: import.meta.env.DEV ? 'development' : env.PUBLIC_ENVIRONMENT || 'production',
 		release: (() => {
 			let release = `${pkg.name}@${pkg.version}`
